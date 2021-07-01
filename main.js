0f67c0fbf0eea74df2d2d6d88fcc3c2123c28563
Simon Fremaux <dievardump@gmail.com>
2021-07-01 22:36:59 +0200

    use beyondHelpers for iNFT v2

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const params =  new URLSearchParams(window.location.search);
-
 // current owner (if known, else default to 0x0)
-const owner = params.get('owner') || '0x0000000000000000000000000000000000000000';
+const owner = beyondHelpers.get('owner', '0x0000000000000000000000000000000000000000');
 // viewer parameter (only if we know about it, else empty so we default to OxO)
-const viewer = params.get('viewer') || '0x0000000000000000000000000000000000000000';
+const viewer = beyondHelpers.get('viewer', '0x0000000000000000000000000000000000000000');
 
 const meta = {
 	owner,
